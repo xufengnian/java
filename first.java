394d4a18522bbdac4e50ffceeb5798e8e1e350d8
xufengnian <cuebwlan@outlook.com>
2016-08-14 11:20:12 +0800

    myfisrt java program
    Signed-off-by: xufengnian <cuebwlan@outlook.com>

---
+++ first.java
@@ -0,0 +1,8 @@
+
+public class first {
+	public static void main (String[] args ){//注意大小写，String的S大写
+		System.out.println("I Rule!");//System的S也是大写
+		System.out.println("The World");//注意区分单引号和双引号
+	}
+
+}
